8d0a09edd3061f90c395b3bec58e980f6fc95d91
nchimunya-joseph <nchimunya.backlogcode@gmail.com>
2023-07-25 10:16:54 +0200

    api removel

--- app.py
+++ app.py
@@ -1,149 +1,124 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 from flask_bcrypt import Bcrypt
 import jwt
+from models import db, User, Auction, Bid
 from datetime import datetime
-from flask_sqlalchemy import SQLAlchemy
+import secrets
+
+secret_key = secrets.token_hex(32)
 
 app = Flask(__name__)
-bcrypt = Bcrypt()
+bcrypt = Bcrypt(app)
 
-app.config['SECRET_KEY'] = 'your-secret-key'
+app.config['SECRET_KEY'] = secret_key
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///auction.db'
 
-db = SQLAlchemy(app)
+db.init_app(app)
+with app.app_context():
+    db.create_all()
 
+@app.route("/")
+def landing_page():
+    return render_template('index.html')
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(50), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), nullable=False)
-
-    def __repr__(self):
-        return f'<User {self.username}>'
-
-
-class Auction(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), nullable=False)
-    description = db.Column(db.Text)
-    initial_bid = db.Column(db.Float, nullable=False)
-    end_date = db.Column(db.DateTime, nullable=False)
-
-    def __repr__(self):
-        return f'<Auction {self.title}>'
-
-
-class Bid(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    auction_id = db.Column(db.Integer, db.ForeignKey('auction.id'), nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    amount = db.Column(db.Float, nullable=False)
-
-    def __repr__(self):
-        return f'<Bid {self.amount}>'
-
-db.create_all()
-
-
-@app.route('/api/users/register', methods=['POST'])
+@app.route('/register', methods=['GET', 'POST'])
 def register():
-    data = request.get_json()
-    username = data.get('username')
-    email = data.get('email')
-    password = data.get('password')
+    if request.method == 'POST':
+        data = request.form
+        username = data.get('username')
+        email = data.get('email')
+        password = data.get('password')
 
-    # Validate input data
-    if not username or not email or not password:
-        return jsonify({'error': 'Invalid input'}), 400
+        # Validate input data
+        if not username or not email or not password:
+            return render_template('register.html', error='Invalid input')
 
-    # Check if username or email already exists
-    existing_user = User.query.filter_by(username=username).first() or User.query.filter_by(email=email).first()
-    if existing_user:
-        return jsonify({'error': 'Username or email already exists'}), 400
+        # Check if username or email already exists
+        existing_user = User.query.filter_by(username=username).first() or User.query.filter_by(email=email).first()
+        if existing_user:
+            return render_template('register.html', error='Username or email already exists')
 
-    # Hash the password
-    hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
+        # Hash the password
+        hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
 
-    # Create user object
-    user = User(username=username, email=email, password=hashed_password)
+        # Create user object
+        user = User(username=username, email=email, password=hashed_password)
 
-    # Store user data
-    db.session.add(user)
-    db.session.commit()
+        # Store user data
+        db.session.add(user)
+        db.session.commit()
 
-    return jsonify({'message': 'User registered successfully'}), 201
+        return render_template('login.html', message='User registered successfully')
 
+    return render_template('register.html')
 
-@app.route('/api/users/login', methods=['POST'])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    data = request.get_json()
-    username = data.get('username')
-    password = data.get('password')
+    if request.method == 'POST':
+        data = request.form
+        username = data.get('username')
+        password = data.get('password')
 
-    # Find user by username
-    user = User.query.filter_by(username=username).first()
+        # Find user by username
+        user = User.query.filter_by(username=username).first()
 
-    if not user:
-        return jsonify({'error': 'Invalid username or password'}), 401
+        if not user:
+            return render_template('login.html', error='Invalid username or password')
 
-    # Check password match
-    if not bcrypt.check_password_hash(user.password, password):
-        return jsonify({'error': 'Invalid username or password'}), 401
+        # Check password match
+        if not bcrypt.check_password_hash(user.password, password):
+            return render_template('login.html', error='Invalid username or password')
 
-    # Generate access token
-    access_token = generate_access_token(user.username)
+        # Generate access token
+        access_token = generate_access_token(user.username)
 
-    return jsonify({'access_token': access_token}), 200
+        # Render the template with access_token as a parameter
+        return render_template('profile.html', access_token=access_token)
 
+    return render_template('login.html')
 
-# Utility function to generate access token
-def generate_access_token(username):
-    payload = {'username': username}
-    access_token = jwt.encode(payload, app.config['SECRET_KEY'], algorithm='HS256')
-    return access_token.decode('utf-8')
+@app.route('/auctions', methods=['GET', 'POST'])
+def auctions():
+    if request.method == 'POST':
+        data = request.form
+        title = data.get('title')
+        description = data.get('description')
+        initial_bid = data.get('initial_bid')
+        end_date = data.get('end_date')
 
-@app.route('/api/auctions',
+        end_date = datetime.fromisoformat(end_date.replace('T', ' '))
+        # Validate input data
+        if not title or not initial_bid or not end_date:
+            return render_template('create_auction.html', error='Invalid input')
 
- methods=['POST'])
-def create_auction():
-    data = request.get_json()
-    title = data.get('title')
-    description = data.get('description')
-    initial_bid = data.get('initial_bid')
-    end_date = data.get('end_date')
+        # Create auction object
+        auction = Auction(title=title, description=description, initial_bid=initial_bid, end_date=end_date)
 
-    # Validate input data
-    if not title or not initial_bid or not end_date:
-        return jsonify({'error': 'Invalid input'}), 400
+        # Store auction data
+        db.session.add(auction)
+        db.session.commit()
 
-    # Create auction object
-    auction = Auction(title=title, description=description, initial_bid=initial_bid, end_date=end_date)
+        return render_template('create_auction.html', message='Auction created successfully')
 
-    # Store auction data
-    db.session.add(auction)
-    db.session.commit()
+    return render_template('create_auction.html')
 
-    return jsonify({'message': 'Auction created successfully'}), 201
-
-
-@app.route('/api/auctions/<int:auction_id>/bids', methods=['POST'])
+@app.route('/auctions/<int:auction_id>/bids', methods=['POST'])
 def place_bid(auction_id):
-    data = request.get_json()
+    data = request.form
     user_id = data.get('user_id')
     bid_amount = data.get('bid_amount')
 
     auction = Auction.query.get(auction_id)
     if not auction:
-        return jsonify({'error': 'Auction not found'}), 404
+        return render_template('place_bid.html', error='Auction not found')
 
     highest_bid = Bid.query.filter_by(auction_id=auction_id).order_by(Bid.amount.desc()).first()
 
     if not bid_amount or bid_amount <= auction.initial_bid:
-        return jsonify({'error': 'Invalid bid amount'}), 400
+        return render_template('place_bid.html', error='Invalid bid amount')
 
     if highest_bid and bid_amount <= highest_bid.amount:
-        return jsonify({'error': 'Bid amount must be higher than the current highest bid'}), 400
+        return render_template('place_bid.html', error='Bid amount must be higher than the current highest bid')
 
     # Create bid object
     bid = Bid(auction_id=auction_id, user_id=user_id, amount=bid_amount)
@@ -152,37 +127,42 @@ def place_bid(auction_id):
     db.session.add(bid)
     db.session.commit()
 
-    return jsonify({'message': 'Bid placed successfully'}), 201
+    return render_template('place_bid.html', message='Bid placed successfully')
 
-
-@app.route('/api/auctions/<int:auction_id>', methods=['PUT'])
+@app.route('/auctions/<int:auction_id>', methods=['GET', 'POST'])
 def update_auction(auction_id):
-    data = request.json
-
     auction = Auction.query.get(auction_id)
     if not auction:
-        return jsonify({'error': 'Auction not found'}), 404
+        return render_template('update_auction.html', error='Auction not found')
 
-    auction.title = data['title']
-    auction.description = data['description']
-    auction.end_date = data['end_date']
+    if request.method == 'POST':
+        data = request.form
+        auction.title = data['title']
+        auction.description = data['description']
+        auction.end_date = data['end_date']
 
-    db.session.commit()
+        db.session.commit()
 
-    return jsonify({'message': 'Auction updated successfully'}), 200
+        return render_template('update_auction.html', auction=auction, message='Auction updated successfully')
 
+    return render_template('update_auction.html', auction=auction)
 
-@app.route('/api/auctions/<int:auction_id>', methods=['DELETE'])
+@app.route('/auctions/<int:auction_id>/delete', methods=['POST'])
 def delete_auction(auction_id):
     auction = Auction.query.get(auction_id)
     if not auction:
-        return jsonify({'error': 'Auction not found'}), 404
+        return render_template('delete_auction.html', error='Auction not found')
 
     db.session.delete(auction)
     db.session.commit()
 
-    return jsonify({'message': 'Auction deleted successfully'}), 200
+    return render_template('delete_auction.html', message='Auction deleted successfully')
 
+# Utility function to generate access token
+def generate_access_token(username):
+    payload = {'username': username}
+    access_token = jwt.encode(payload, app.config['SECRET_KEY'], algorithm='HS256')
+    return access_token
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
---
+++ models.py
@@ -0,0 +1,31 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(80), nullable=False)
+
+    def __repr__(self):
+        return f'<User {self.username}>'
+
+class Auction(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text)
+    initial_bid = db.Column(db.Float, nullable=False)
+    end_date = db.Column(db.DateTime, nullable=False)
+
+    def __repr__(self):
+        return f'<Auction {self.title}>'
+
+class Bid(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    auction_id = db.Column(db.Integer, db.ForeignKey('auction.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    amount = db.Column(db.Float, nullable=False)
+
+    def __repr__(self):
+        return f'<Bid {self.amount}>'
