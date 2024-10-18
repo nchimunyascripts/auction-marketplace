51c83d39569f26124bee0b1ba37b1b195af36bcb
nchimunyascripts <nchimunya.backlogcode@gmail.com>
2024-10-18 03:52:26 +0000

    run update 23

--- run.py
+++ run.py
@@ -1,4 +1,10 @@
-<<<<<<< HEAD
+>>>>>>> parent of 0868c01... Update run.py
 =======
+from auction_app import create_app
+
+app = create_app()
+
+if __name__ == '__main__':
+    app.run(debug=True)
     
 >>>>>>> parent of 0868c01... Update run.py
